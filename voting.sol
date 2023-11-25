f07f67652b61421dad1d8f6a0f82ac02a587b3e9
Juanda16 <36868064+Juanda16@users.noreply.github.com>
2023-11-25 16:27:48 -0500

    add comments to explain the code

--- voting.sol
+++ voting.sol
@@ -1,34 +1,52 @@
 pragma solidity ^0.6.6;
 
-contract Voting{
-    
-    struct Candidate{
-        uint id;
-        string name;
-        uint voteCount;
+contract Voting {
+    // Define a struct to represent a candidate
+    struct Candidate {
+        uint id; // Unique identifier for the candidate
+        string name; // Name of the candidate
+        uint voteCount; // Number of votes the candidate has received
     }
-    
-    mapping (uint => Candidate) public candidates;
+
+    // Mapping to store candidate information
+    mapping(uint => Candidate) public candidates;
+
+    // Variable to keep track of the total number of candidates
     uint public candidatecount;
-    mapping (address => bool) public citizen;
-    
-    constructor() public{
+
+    // Mapping to track whether a user has voted or not
+    mapping(address => bool) public citizen;
+
+    // Constructor function to initialize the contract
+    constructor() public {
+        // Add initial candidates
         addCandidate("Juan");
         addCandidate("Laura");
         addCandidate("Jazmin");
         addCandidate("Ramiro");
     }
-    
-    function addCandidate(string memory _name) private{
+
+    // Function to add a new candidate
+    function addCandidate(string memory _name) private {
+        // Increment the candidate count
         candidatecount++;
-        candidates[candidatecount] = Candidate(candidatecount, _name, 0);
+
+        // Create a new candidate struct
+        Candidate memory newCandidate = Candidate(candidatecount, _name, 0);
+
+        // Add the new candidate to the mapping
+        candidates[candidatecount] = newCandidate;
     }
-    
-    function vote(uint _candidateid) public{
-        require(!citizen[msg.sender]);
-        
+
+    // Function to cast a vote for a candidate
+    function vote(uint _candidateid) public {
+        // Check if the user has already voted
+        require(!citizen[msg.sender], "User has already voted");
+
+        // Mark the user as having voted
         citizen[msg.sender] = true;
-        candidates[_candidateid].voteCount ++;
-        
+
+        // Increment the vote count for the selected candidate
+        candidates[_candidateid].voteCount++;
     }
 }
